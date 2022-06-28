e6a12efb4dada7347f8d497f6d40f18924eb58d7
Ioan Lazar <ilazar@8x8.com>
2022-06-28 10:47:19 +0300

    Run from current - v5.0.0

---
+++ copy-to-vendor.cmd
@@ -0,0 +1,19 @@
+xcopy /s ..\Squirrel.Windows\build\Release\Win32\Setup.exe vendor\Setup.exe /Y
+xcopy /s ..\Squirrel.Windows\build\Release\Win32\Setup.pdb vendor\Setup.pdb /Y
+
+xcopy /s ..\Squirrel.Windows\build\Release\Win32\StubExecutable.exe vendor\StubExecutable.exe /Y
+xcopy /s ..\Squirrel.Windows\build\Release\Win32\StubExecutable.pdb vendor\StubExecutable.pdb /Y
+
+xcopy /s ..\Squirrel.Windows\build\Release\Win32\WriteZipToSetup.exe vendor\WriteZipToSetup.exe /Y
+xcopy /s ..\Squirrel.Windows\build\Release\Win32\WriteZipToSetup.pdb vendor\WriteZipToSetup.pdb /Y
+
+xcopy /s ..\Squirrel.Windows\build\Release\net45\update.com vendor\Squirrel.com /Y
+xcopy /s ..\Squirrel.Windows\build\Release\net45\Update.exe vendor\Squirrel.exe /Y
+xcopy /s ..\Squirrel.Windows\build\Release\net45\Update.pdb vendor\Squirrel.pdb /Y
+
+xcopy /s ..\Squirrel.Windows\build\Release\net45\Update-Mono.exe vendor\Squirrel-Mono.exe /Y
+xcopy /s ..\Squirrel.Windows\build\Release\net45\Update-Mono.pdb vendor\Squirrel-Mono.pdb /Y
+
+xcopy /s ..\Squirrel.Windows\build\Release\net45\SyncReleases.exe vendor\SyncReleases.exe /Y
+xcopy /s ..\Squirrel.Windows\build\Release\net45\SyncReleases.pdb vendor\SyncReleases.pdb /Y
+
